269bdbad6033854a068a232d1d767e79ea244fb3
RIG360 <134787168+RIG360@users.noreply.github.com>
2026-09-14 15:01:42 +0200

    commit

---
+++ data.js
@@ -0,0 +1,219 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-breit27-2-31_hausflur",
+      "name": "Breit27-2-31_Hausflur",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8839297857856234,
+          "pitch": 0.01824281749337331,
+          "rotation": 0,
+          "target": "1-breit27-2-31_kuche1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-breit27-2-31_kuche1",
+      "name": "Breit27-2-31_Küche1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.057771803493986695,
+          "pitch": 0.06694288454783148,
+          "rotation": 0,
+          "target": "0-breit27-2-31_hausflur"
+        },
+        {
+          "yaw": 1.5734550729155838,
+          "pitch": 0.061036159440682525,
+          "rotation": 0,
+          "target": "2-breit27-2-31_bad"
+        },
+        {
+          "yaw": -3.1164443077026807,
+          "pitch": -0.009828731712152816,
+          "rotation": 0,
+          "target": "3-breit27-2-31_kuche2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-breit27-2-31_bad",
+      "name": "Breit27-2-31_Bad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5208629724923561,
+          "pitch": 0.06497397617878065,
+          "rotation": 0,
+          "target": "1-breit27-2-31_kuche1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-breit27-2-31_kuche2",
+      "name": "Breit27-2-31_Küche2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-breit27-2-31_kuche1"
+        },
+        {
+          "yaw": -3.114630663421062,
+          "pitch": 0.03935287968320189,
+          "rotation": 0,
+          "target": "4-breit27-2-31_wohnzimmer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-breit27-2-31_wohnzimmer",
+      "name": "Breit27-2-31_Wohnzimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.15116770064302365,
+          "pitch": 0.0029527869069827517,
+          "rotation": 0,
+          "target": "3-breit27-2-31_kuche2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Breit27-2-31_260912.JPEG",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
